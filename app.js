f4e086d493bceb254fe269922781e1e596d613bb
Latorreander <latorreander@gmail.com>
2023-05-18 19:15:32 -0300

    'nome variável de contagem de imagens alterado'

--- app.js
+++ app.js
@@ -3,33 +3,34 @@ const images = document.querySelectorAll(".image");
 const forwardButton = document.getElementById("forward-btn");
 const backButton = document.getElementById("back-btn");
 
-let counter = 0;
+let counterImage = 0;
+
 currentImage = images.length.classList = "active";
 
 forwardButton.addEventListener("click", () => {
     if (currentImage) {
-        images[counter].classList.remove("active");
-        counter++;
-        images[counter].classList.add("active");
+        images[counterImage].classList.remove("active");
+        counterImage++;
+        images[counterImage].classList.add("active");
         backButton.classList.remove("disable");
         backButton.classList.add("enable");
     }
 
-    if (counter === images.length - 1) {
+    if (counterImage === images.length - 1) {
         forwardButton.classList.add("disable");
     }
 });
 
 backButton.addEventListener("click", () => {
     if (currentImage) {
-        images[counter].classList.remove("active");
-        counter--;
-        images[counter].classList.add("active");
+        images[counterImage].classList.remove("active");
+        counterImage--;
+        images[counterImage].classList.add("active");
         forwardButton.classList.add("enable");
         forwardButton.classList.remove("disable");
     }
 
-    if (counter === 0) {
+    if (counterImage === 0) {
         backButton.classList.add("disable");
         forwardButton.classList.add("enable");
     }
